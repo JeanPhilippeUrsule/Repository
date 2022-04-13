d4e50bccde3dbc7718d2a95317d9f74f7f839874
Jean-Philippe <jeanphilippe.ursule@gmail.com>
2022-04-13 02:20:50 -0300

    Creacion de las vistas

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,151 @@
+from projects.models import Project, Paper, Tutor
+from django.http import HttpResponse
 from django.shortcuts import render
+from projects.forms import ProjectFormulary, TutorFormulary, PaperFormulary
 
 # Create your views here.
+
+#Vamos a tener 2 tipos de vistas, una general y una detallada
+
+#Vistas generales 
+
+def index(request):
+    return render(request,'projects/index.html')
+
+def project_index(request):
+    projects = Project.objects.all()#levantamos todos los objetos de la class Project
+    context = {
+        'projects': projects
+    }#creamos el diccionario
+    return render(request, 'project_index.html', context) #linkeamos el diccionario con un html
+
+def paper_index(request):
+    papers = Paper.objects.all()
+    context = {
+        'papers': papers
+    }
+        
+    return render(request,'paper_index.html', context)
+
+def tutor_index(request):
+    tutors = Tutor.objects.all()
+    context = {
+        'tutors': tutors
+    }
+    
+    return render(request,'projects/tutor_index.html',context)
+
+#Vistas detalladas 
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)#el pk nos permite usar el numero del projecto va ser util en un loop
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
+
+def paper_detail(request,pk):
+    paper = Paper.objects.get(pk=pk)
+    context = {
+        'paper': paper
+    }
+        
+    return render(request,'paper_detail.html', context)
+
+def tutor_detail(request,pk):
+    tutor = Tutor.objects.get(pk=pk)
+    context = {
+        'tutor': tutor
+    }
+        
+    return render(request,'tutor_detail.html', context)
+
+#Formularios
+
+def projectFormulary(request):
+    
+    if request.method == 'POST': #Vamos el metodo POST
+        
+        myFormulary = ProjectFormulary(request.POST)
+        
+        if myFormulary.is_valid(): #comprobamos que cumplimos los requisitos
+            
+            information = myFormulary.cleaned_data #vamos a acceder a este informacion como diccionario
+            
+            project = Project(title = information['title'], 
+                              date_start = information['date_start'],
+                              date_end = information['date_end'],
+                              resume = information['resume'],
+                              description = information['description'],
+                              technology = information['technology']
+                              )#datos del proyecto
+            project.save()#guardamos todo
+            return render(request,'projects/project_index.html')
+    else:
+        myFormulary= ProjectFormulary()
+        
+    return render(request, 'projects/projectFormulary.html',{'myFormulary':myFormulary})
+
+def tutorFormulary(request):
+    
+    if request.method == 'POST':
+        
+        myFormulary = TutorFormulary(request.POST)
+        
+        if myFormulary.is_valid():
+            
+            information = myFormulary.cleaned_data #vamos a cceder a este informacion como diccionario
+
+            tutor = Tutor(name_student = information['name_student'],
+                          title = information['title'], 
+                          date_start = information['date_start'],
+                          date_end = information['date_end'],
+                          description = information['description']
+                          )
+            tutor.save()
+            
+            return render(request,'projects/tutor_index.html')
+    else:
+        myFormulary= TutorFormulary()
+        
+    return render(request, 'projects/tutorFormulary.html',{'myFormulary':myFormulary})
+
+def paperFormulary(request):
+    
+    if request.method == 'POST':
+        
+        myFormulary = PaperFormulary(request.POST)
+        
+        if myFormulary.is_valid():
+            
+            information = myFormulary.cleaned_data #vamos a cceder a este informacion como diccionario
+
+            paper = Paper(title = information['title'], 
+                          date = information['date'],
+                          description = information['description'],
+                          )
+            
+            paper.save()
+            return render(request,'projects/paper_index.html')
+    else:
+        myFormulary= PaperFormulary()
+        
+    return render(request, 'projects/paperFormulary.html',{'myFormulary':myFormulary})
+
+#Buscador
+
+def searchProject(request):
+    
+    if request.GET['title']: #buscamos con elm metodo GET el titulo
+        title= request.GET['title']#Variable titulo
+        project= Project.objects.filter(title__icontains=title)#variable del buscador
+        
+        return render(request, 'projects/index.html', {'project':project, 'query':title})
+    
+    else:
+        answer='No data send'
+    
+    return render(request, 'projects/index.html', {'answer':answer})
+
+def projectSearch(request):
+    return render(request, 'projects/index.html')
